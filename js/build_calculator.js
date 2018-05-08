531031444fd7bf5a8546c45a5f08d1b3897c3318
Eli Lisseck <lisseck@gmail.com>
2018-05-07 18:28:59 -0700

    view post render fxn

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,7 +1,6 @@
 (function ($) {
     Drupal.behaviors.build_calculator = {
       attach: function (context, settings) {
-        alert('hello world');
          console.log(settings);
       }
     };
