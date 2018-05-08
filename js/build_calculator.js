bcec2e81298f87fdf1a8e1a14ede8164cd783990
Eli Lisseck <lisseck@gmail.com>
2018-05-08 10:00:30 -0700

    add complexity to example js

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,7 +1,11 @@
 (function ($, Drupal) {
   Drupal.behaviors.build_calculator = {
     attach: function (context, settings) {
-      console.log(settings);
+      $("#edit-hero, #edit-level, #edit-items", context).once('build_calculator').each(function() {
+	    $(this).change(function() {
+          console.log("changed input");
+        });
+	  });
     }
   };
 })(jQuery, Drupal);
