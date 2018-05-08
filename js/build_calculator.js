04f9c78d44ad8a0c1e8826b25bf254da1f5960aa
Eli Lisseck <lisseck@gmail.com>
2018-05-08 09:53:11 -0700

    simplify further

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,11 +1,7 @@
 (function ($, Drupal) {
   Drupal.behaviors.build_calculator = {
     attach: function (context, settings) {
-      $("#edit-hero, #edit-level, #edit-items", context).once('build_calculator').each(function() {
-	    $(this).change(function() {
-          console.log("changed input");
-        });
-	  });
+      console.log(settings);
     }
   };
 })(jQuery, Drupal);
