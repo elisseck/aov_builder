a96de1a75cca6922f62a990c12f96f2ba38ae12d
Eli Lisseck <lisseck@gmail.com>
2018-05-08 09:32:24 -0700

    try another js style

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,15 +1,11 @@
 (function ($) {
-  function elementWatchers(context, settings) {
-	$(context).find("#edit-hero, #edit-level, #edit-items").once("build_calculator").each(function() {
-	  $(this).change(function() {
-        console.log("changed input" + this);
-      });
-	});
-  }
-	
   Drupal.behaviors.build_calculator = {
     attach: function (context, settings) {
-      elementWatchers();
+      $(context).find("#edit-hero, #edit-level, #edit-items").once("build_calculator").each(function() {
+	    $(this).change(function() {
+          console.log("changed input" + this);
+        });
+	  });
     }
   };
 })(jQuery);
