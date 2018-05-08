f2437685e15145f06f3514a2268e23a4336b6e88
Eli Lisseck <lisseck@gmail.com>
2018-05-08 09:21:47 -0700

    register change events

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,7 +1,15 @@
-(function ($) {
-    Drupal.behaviors.build_calculator = {
-      attach: function (context, settings) {
-         console.log(settings);
-      }
-    };
+(function ($, Drupal) {
+  function elementWatchers(context) {
+	$(context).find("#edit-hero, #edit-level, #edit-items").once("build_calculator").each(function() {
+	  $(this).change(function() {
+        console.log("changed input" + this);
+      });
+	});
+  }
+	
+  Drupal.behaviors.build_calculator = {
+    attach: function (context, settings) {
+      elementWatchers();
+    }
+  };
 })(jQuery);
