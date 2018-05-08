31aae17c17673189d1cbe0622c09ab3ecd9bd85f
Eli Lisseck <lisseck@gmail.com>
2018-05-08 13:07:08 -0700

    init fullbuild with numbers

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,7 +1,6 @@
 (function ($, Drupal) {
   Drupal.behaviors.build_calculator = {
     attach: function (context, settings) {
-	  console.log(settings.heroData);
       $("#edit-hero, #edit-level, #edit-items", context).once('build_calculator').each(function() {
 	    $(this).change(function() {
           generateBuild(settings);
@@ -11,45 +10,37 @@
   };
   
   function generateBuild(settings) {
-	var fullBuild = {};
+	//initialize build stats as numbers
+	var fullBuild = {
+	  "field_ability_power": 0,
+      "field_armor": 0,
+      "field_attack_damage": 0,
+      "field_attack_speed": 0,
+      "field_cdr": 0,
+      "field_crit_chance": 0,
+      "field_flat_pen_ad": 0,
+      "field_flat_pen_ap": 0,
+      "field_hp": 0,
+      "field_hp_regen_5_seconds": 0,
+      "field_life_steal": 0,
+      "field_magic_defense": 0,
+      "field_magic_life_steal": 0,
+      "field_mana": 0,
+      "field_mana_regen_5_seconds": 0,
+      "field_movement_speed": 0,
+      "field_percent_pen_ad": 0,
+      "field_percent_pen_ap": 0,
+	  "field_critical_damage": 0,
+	};
 	var selectedHero = $("#edit-hero").val();
 	var selectedLevel = $("#edit-level").val();
 	var selectedItems = $("#edit-items").val();
-	var heroKeys = [
-      "field_ability_power",
-      "field_ad_per_level",
-      "field_armor",
-      "field_armor_per_level",
-      "field_as_per_level",
-      "field_attack_damage",
-      "field_attack_speed",
-      "field_cdr",
-      "field_crit_chance",
-      "field_critd_per_level",
-      "field_critical_damage",
-      "field_flat_pen_ad",
-      "field_flat_pen_ap",
-      "field_hp",
-      "field_hp5_per_level",
-      "field_hp_per_level",
-      "field_hp_regen_5_seconds",
-      "field_life_steal",
-      "field_magic_defense",
-      "field_magic_life_steal",
-      "field_mana",
-      "field_mana5_per_level",
-      "field_mana_per_level",
-      "field_mana_regen_5_seconds",
-      "field_md_per_level",
-      "field_movement_speed",
-      "field_percent_pen_ad",
-      "field_percent_pen_ap",
-      ];
 	//apply arcana first
 	//apply items next
-	console.log(selectedItems);
 	fullBuild = addItems(settings.itemData, selectedItems, fullBuild);
+	//apply level scaling
 	fullBuild = scaleByLevel(settings.heroData[selectedHero], selectedLevel, fullBuild);
+	//output
 	var buildContainer = $(".full_build");
 	buildContainer.empty();
 	for (var data in fullBuild) {
@@ -82,6 +73,7 @@
   }
   
   function addItems(items, selectedItems, fullBuild) {
+	console.log(items);
 	for (var item in selectedItems) {
 	  if (items.hasOwnProperty(item)) {
 		for (var key in items[item]) {
@@ -91,7 +83,6 @@
 		}
 	  }
 	}
-    //do item stuff
 	return fullBuild;
   }
 })(jQuery, Drupal);
