2965f9736613f6664a96744647a95bf99d87e4db
Eli Lisseck <lisseck@gmail.com>
2018-05-08 09:51:49 -0700

    simplified example

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,9 +1,9 @@
 (function ($, Drupal) {
   Drupal.behaviors.build_calculator = {
     attach: function (context, settings) {
-      $("#edit-hero, #edit-level, #edit-items", context).once('myCustomBehavior').find("#edit-hero, #edit-level, #edit-items").each(function() {
+      $("#edit-hero, #edit-level, #edit-items", context).once('build_calculator').each(function() {
 	    $(this).change(function() {
-          console.log("changed input" + this);
+          console.log("changed input");
         });
 	  });
     }
