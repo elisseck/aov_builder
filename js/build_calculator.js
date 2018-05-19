271dcc4220e6796be3681769d2b99305181d23bb
Eli Lisseck <lisseck@gmail.com>
2018-05-18 20:17:15 -0700

    first pass at skills processing

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -1,7 +1,7 @@
 (function ($, Drupal) {
   Drupal.behaviors.build_calculator = {
     attach: function (context, settings) {
-      $("#edit-hero, #edit-level, .edit-items, .edit-arcana", context).once('build_calculator').each(function() {
+      $("#edit-hero, #edit-level, .edit-items, .edit-arcana, .edit-skill-levels", context).once('build_calculator').each(function() {
 	    $(this).change(function() {
           generateBuild(settings);
         });
@@ -45,14 +45,14 @@
     }).get().join(',');
 	//get base stats
 	fullBuild = getBaseStats(settings.heroData[selectedHero], fullBuild);
-	//get skills
-	skillBuild = getSkills(settings.skillAndBonusData[selectedHero], selectedSkillLevels);
 	//apply arcana first
 	fullBuild = addArcana(settings.arcanaData, selectedArcana, fullBuild);
 	//apply items next
 	fullBuild = addItems(settings.itemData, selectedItems, fullBuild);
 	//apply level scaling
 	fullBuild = scaleByLevel(settings.heroData[selectedHero], selectedLevel, fullBuild);
+	//get skills last because they require fullBuild data
+	skillBuild = getSkills(settings.skillAndBonusData[selectedHero], selectedSkillLevels, selectedHero, selectedLevel, fullBuild);
 	//output to visible container and hidden container to separate nice markup vs easy data values
 	var buildContainer = $(".full_build");
 	var hiddenContainer = $('input[name="full_build_hidden"]');
@@ -82,21 +82,46 @@
 	return fullBuild;
   }
   
-  function getSkills(skills, levels) {
+  function getSkills(skills, levels, hero, heroLevel, fullBuild) {
+	levels = levels.split(',');
 	var skillScales = {
 	  "field_cooldown": "field_cooldown_per_level",
 	  "field_level_1": "field_skill_damage_per_level",
+	  "field_scaling": "field_scaling_stat",
 	}
 	var bonusScales = {
 	  "field_bonus_damage_level_1": "field_bonus_damage_per_level",
+	  "field_scaling": "field_scaling_stat",
 	}
-	console.log(skills);
-	console.log(levels);
-	for (var key in skillScales) {
-	  if (skillScales.hasOwnProperty(key)) {
-	    //
+	for var (skill in skills) {
+	  for var (key in skill) {
+		//a ton of sanity checks because we really have no idea what's coming in here
+	    if (skill.hasOwnProperty(key)) {
+		  if (skill[key]['values'].hasOwnProperty(0)) {
+			if (skill[key]['values'][0].hasOwnProperty('value') {
+			  if (skillScales.hasOwnProperty(key)) {
+				//if it's a scaling field, scale value by the current fullBuild value for the scaling stat
+				if (key == 'field_scaling') {
+				  $skillBuild[skill][key] = parseFloat(skill[key]['values'][0]['value']) * parseFloat(fullBuild[skill['field_scaling_stat']]);
+				} else {
+				//cases for each skill type so we scale by the correct level for level scaling
+				  if (skill['field_skill_type']['values'][0]['value'] == 'Passive') {
+				    $skillBuild[skill][key] = parseFloat(skill[key]['values'][0]['value']) + (parseFloat(skill[skillScales[key]]['values'][0]['value']) * parseFloat(heroLevel));
+				  } else if (skill['field_skill_type']['values'][0]['value'] == 'Skill 1') {
+				    $skillBuild[skill][key] = parseFloat(skill[key]['values'][0]['value']) + (parseFloat(skill[skillScales[key]]['values'][0]['value']) * parseFloat(levels[0]));
+				  } else if (skill['field_skill_type']['values'][0]['value'] == 'Skill 2') {
+				    $skillBuild[skill][key] = parseFloat(skill[key]['values'][0]['value']) + (parseFloat(skill[skillScales[key]]['values'][0]['value']) * parseFloat(levels[1]));
+				  } else {
+				    $skillBuild[skill][key] = parseFloat(skill[key]['values'][0]['value']) + (parseFloat(skill[skillScales[key]]['values'][0]['value']) * parseFloat(levels[2]));
+				  }
+			    }
+			  }
+			}
+		  }
+		}
 	  }
 	}
+	return skillBuild;
   }
   
   function scaleByLevel(hero, selectedLevel, fullBuild) {
