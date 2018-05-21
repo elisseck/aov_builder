347aaf60ad112e5dd5f2c9b35f842195e69b4014
Eli Lisseck <lisseck@gmail.com>
2018-05-21 15:29:14 -0700

    same for bonus

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -220,7 +220,9 @@
 	  markup += '<div class="skill-output-type">Output Type: ' + skillBuild[skill]['field_output_type'] + '</div>';
 	  markup += '</div><div class="bonuses"><h4>Additional Effects</h4>';
 	  for (var bonus in skillBuild[skill]['bonuses']) {
-	    markup += '<div class="bonus-description">' + skillBuild[skill]['bonuses'][bonus]['body'][0]['value'] + '></div>';
+		if (skillBuild[skill]['bonuses'][bonus]['body'].hasOwnProperty(0)) {
+	      markup += '<div class="bonus-description">' + skillBuild[skill]['bonuses'][bonus]['body'][0]['value'] + '></div>';
+		}
 	  }
 	  markup += '</div>'
     }
