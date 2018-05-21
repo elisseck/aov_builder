0762c45e76379d5627397bd3c2b54ae27de18c52
Eli Lisseck <lisseck@gmail.com>
2018-05-21 14:04:10 -0700

    missed +

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -206,7 +206,7 @@
   function appendToSkillContainer(container, skillBuild) {
     var markup = '<div id="skills-final">';
 	for (var skill in skillBuild) {
-	  markup += '<div class="skill-title"><h3>' skill['field_skill_type'][0]['value'] + ' - ' + skill['title'] + '</h3></div>';
+	  markup += '<div class="skill-title"><h3>' + skill['field_skill_type'][0]['value'] + ' - ' + skill['title'] + '</h3></div>';
 	  markup += '<div class="skill-description>' + skill['body'][0]['value'].text() + '</div>';
 	  markup += '<div class="skill-stats>';
 	  markup += '<div class="skill-scaling">Scaling Value: ' + skill['field_scaling'] + '</div>';
