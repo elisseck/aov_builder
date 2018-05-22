8362ac46b89ba36404b4f62db51ec96664daacf3
Eli Lisseck <lisseck@gmail.com>
2018-05-21 17:47:47 -0700

    more bonus data

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -224,6 +224,10 @@
 	    for (var bonus in skillBuild[skill]['bonuses']) {
 		  if (skillBuild[skill]['bonuses'][bonus]['body'].hasOwnProperty(0)) {
 	        markup += '<div class="bonus-description">' + skillBuild[skill]['bonuses'][bonus]['body'][0]['value'] + '</div>';
+			markup += '<div class="bonus-scaling">Scaling Value: ' + skillBaseData[skill]['bonuses'][bonus]['field_scaling']['values'][0]['value'] + '</div>';
+			markup += '<div class="bonus-scaling-stat">Scaling Stat: ' + skillBuild[skill]['bonuses'][bonus]['field_scaling_stat'] + '</div>';
+			markup += '<div class="bonus-output-value">Output Value: ' + skillBuild[skill]['bonuses'][bonus]['final_value'] + '</div>';
+			markup += '<div class="bonus-output-type">Output Type: ' + skillBuild[skill]['bonuses'][bonus]['field_output_type'] + '</div>';
 		  }
 	    }
 		markup += '</div>';
@@ -268,6 +272,9 @@
 		  }
 		}	
 	  }
+	  bonusBuild[bonus]['field_scaling_stat'] = bonuses[bonus]['field_scaling_stat']['termname'];
+	  bonusBuild[bonus]['field_output_type'] = bonuses[bonus]['field_output_type']['termname'];
+	  bonusBuild[bonus]['final_value'] = parseFloat(bonusBuild[bonus]['field_scaling']) + parseFloat(bonusBuild[bonus]['field_level_1']);
 	}
 	return bonusBuild;
   }
