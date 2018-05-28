273a87b456a48f83785ab7cf205325b075a6fb5f
Eli Lisseck <lisseck@gmail.com>
2018-05-28 15:09:35 -0700

    proper trigger on load

--- js/build_comparison.js
+++ js/build_comparison.js
@@ -1,8 +1,11 @@
 (function ($, Drupal) {
   Drupal.behaviors.build_comparison = {
     attach: function (context, settings) {
-	  var param = urlParams.get('build');
-      $("#edit-build-1").val(param).trigger('change');
     }
   };
+  $( document ).ready(function() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var param = urlParams.get('build');
+    $("#edit-build-1").val(param).trigger('change');
+  });
 })(jQuery, Drupal);
