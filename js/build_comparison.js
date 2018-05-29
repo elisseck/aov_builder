5da7fa7fe9e76661f4d64b0b814660df75a75830
Eli Lisseck <lisseck@gmail.com>
2018-05-28 17:08:09 -0700

    disable select

--- js/build_comparison.js
+++ js/build_comparison.js
@@ -7,5 +7,6 @@
     var urlParams = new URLSearchParams(window.location.search);
     var param = urlParams.get('build');
     $("#edit-build-1").val(param).trigger('change');
+	$("#edit-build-1").select2().enable(false);
   });
 })(jQuery, Drupal);
