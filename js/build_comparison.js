bf002972468ca6ab24e01febe63b96b32478d216
Eli Lisseck <lisseck@gmail.com>
2018-06-01 16:30:16 -0700

    .

--- js/build_comparison.js
+++ js/build_comparison.js
@@ -1,12 +1,12 @@
 (function ($, Drupal) {
   Drupal.behaviors.build_comparison = {
     attach: function (context, settings) {
+	  $("#edit-build-1").attr("disabled", true);
     }
   };
   $(document).ready(function() {
     var urlParams = new URLSearchParams(window.location.search);
     var param = urlParams.get('build');
     $("#edit-build-1").val(param).trigger('change');
-	$("#edit-build-1").attr("disabled", true);
   });
 })(jQuery, Drupal);
