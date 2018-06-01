a2e72d1ed6186054f5ad124a09df043f1ddecc56
Eli Lisseck <lisseck@gmail.com>
2018-06-01 16:39:29 -0700

    move js

--- js/build_comparison.js
+++ js/build_comparison.js
@@ -2,6 +2,7 @@
   Drupal.behaviors.build_comparison = {
     attach: function (context, settings) {
 	  $("#edit-build-1").attr("disabled", true);
+	  $("#ajax_placeholder_build_1, #ajax_placeholder_build_2").insertAfter(".js-form-item-build-2");
     }
   };
   $(document).ready(function() {
