a6558eff81dc27a77ef085dae293b7118e04003d
Eli Lisseck <lisseck@gmail.com>
2018-06-08 22:38:38 -0700

    indicators

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -58,6 +58,7 @@
 	var buildContainer = $(".full_build");
 	for (var c in buildContainer[0].childNodes) {
 	  console.log(buildContainer[0].childNodes[c]);
+	  console.log($(buildContainer[0].childNodes[c]));
 	  if ($(buildContainer[0].childNodes[c]).className == 'hero-grouping') {
 	    for (var d in $(buildContainer[0].childNodes[c]).childNodes) {
 	      console.log($(buildContainer[0].childNodes[c]).childNodes[d]);
