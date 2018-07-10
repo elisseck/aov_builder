1f0588603a68d5d2350e4697f35fd2723c35629f
Eli Lisseck <lisseck@gmail.com>
2018-07-10 15:09:27 -0700

    first pass at level slider

--- js/build_comparison.js
+++ js/build_comparison.js
@@ -78,7 +78,7 @@
 	  for (var key in fullBuild) {
 	    if (id == key) {
 		  console.log(id);
-	      console.log($(this).textContent);
+	      console.log($(this)[0].textContent);
 		}
 	  }
     });
