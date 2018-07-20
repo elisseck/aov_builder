5504cf5a851f3cc584ef8402d0a033cd79f22f0a
Eli Lisseck <lisseck@gmail.com>
2018-07-19 19:57:01 -0700

    typo

--- js/build_calculator.js
+++ js/build_calculator.js
@@ -273,7 +273,7 @@
 				  //if it's a scaling field, scale value by the current fullBuild value for the scaling stat
 				  if (key == 'field_scaling' && passiveData[item].hasOwnProperty('field_scaling_stat')) {
 				    passiveValue[item][passiveData[item]['field_output_stat']['values']] = parseFloat(passiveData[item][key]['values'][0]['value']) * parseFloat(fullBuild[passiveData[item]['field_scaling_stat']['values']]);
-				  } else if (key == 'field_bonus_damage_level_1' && passiveData[item].hasOwnProperty('field_bonus_damage_per_level')) {
+				  } else if (key == 'field_bonus_damage_level_1' && passiveData[item].hasOwnProperty('field_bonus_damage_per_level') && passiveData[item].hasOwnProperty(key)) {
 					passiveValue[item][passiveData[item]['field_output_stat']['values']] = parseFloat(passiveData[item][key]['values'][0]['value']) + (parseFloat(passiveData[item]['field_bonus_damage_per_level']['values'][0]['value']) * (parseFloat(selectedLevel) - 1)); 
 				  }
 				}
